d8b39058fade78cea4bf6ae0167d3da54259bc8b
Akshay Birajdar <akshayb@joshsoftware.com>
2020-04-01 11:15:10 +0530

    Add reducer and use reducer

--- src/components/LoginFormComponent.js
+++ src/components/LoginFormComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Label, Form, FormGroup, Button, Input } from 'reactstrap';
 import * as yup from 'yup'; // for everything
 
@@ -7,23 +7,48 @@ let schema = yup.object().shape({
   password: yup.string().required(),
 });
 
-const checkValidity = (e) => {
-  debugger;
-  schema.isValid({
-    name: e.target
-  }).then((valid) => { console.log(valid); } )
+const initialState = { email: '', password: '' };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setEmail':
+      return {email: action.value};
+    case 'setPassword':
+      return {password: action.value};
+    default:
+      throw new Error();
+  }
+}
+
+const checkValidity = (state, e) => {
+  // debugger;
+  schema.isValid(state).then((valid) => { console.log(valid); } )
 };
 
 const LoginFormComponent = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return(
-    <Form onSubmit={checkValidity}>
+    <Form onSubmit={checkValidity(state)}>
       <FormGroup>
         <Label for="email">Email</Label>
-        <Input type="email" id="email" name="email" placeholder="Enter Email" />
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Email"
+          onChange={(e)=>{ dispatch({type: 'setEmail', value: e.target.value}) }}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="password">Password</Label>
-        <Input type="password" name="password" id="password" placeholder="Enter password" />
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter password"
+          onChange={(e)=>{ dispatch({type: 'setPassword', value: e.target.value}) }}
+        />
       </FormGroup>
       <Button>Submit</Button>
     </Form>
